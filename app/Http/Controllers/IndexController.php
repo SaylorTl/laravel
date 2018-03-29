2d57eb6dcc48258cfc943ca0878a0f094f0f471d
shitingle <694475241@qq.com>
2018-03-29 18:28:10 +0800

    fixed

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Predis;
+
+class IndexController extends Controller
+{
+    //
+    public function index(){
+        $single_server = array(
+            'host' => '118.24.80.47',
+            'port' => 6379,
+            'database' => 15,
+        );
+        $client = new Predis\Client($single_server + array('read_write_timeout' => 0));
+        $client->set('foo', 'bar');
+        $client->get('foo');
+        print_r($client);exit;
+    }
+}
