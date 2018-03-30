6b63b697c9d52c4c5b056dbd6145b2bbdd828380
shitingle <694475241@qq.com>
2018-03-30 09:12:47 +0800

    fixed

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,14 +10,14 @@ class IndexController extends Controller
     //
     public function index(){
         $single_server = array(
-            'host' => '118.24.80.47',
+            'host' => '127.0.0.1',
             'port' => 6379,
             'database' => 15,
         );
 
         $client  = new Predis\Client($single_server);
         $client->set('foo', 'bar');
-        $client->get('foo');
-        print_r($client);exit;
+        $res = $client->get('foo');
+        print_r($res);exit;
     }
 }
