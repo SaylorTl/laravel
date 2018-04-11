656bf1cbafe8d27c5ca17c55202264c8081e97b3
shitingle <694475241@qq.com>
2018-04-11 09:16:44 +0800

    fixed

--- app/Console/Commands/Ppdai.php
+++ app/Console/Commands/Ppdai.php
@@ -65,10 +65,10 @@ class Ppdai extends Command
         $date = date("Y-m-d H:i:s",time()-3600);
         $request = '{"PageIndex":"'.$this->PageIndex.'","StartDateTime": "'.$date.'"}';
         $result = json_decode($this->client->send($url, $request,config('app.accessToken'),30),true);
-        if($this->PageIndex >= 6){
-            $this->finish = false;
-            return;
-        }
+//        if($this->PageIndex >= 6){
+//            $this->finish = false;
+//            return;
+//        }
         if(!$result){
             pp_log("查询失败：".$result['ResultMessage']);
             $this->finish = false;
