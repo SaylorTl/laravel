492c12fbc6d71be245f9ec4c30b1a7c7bc0cba66
shitingle <694475241@qq.com>
2018-05-08 09:11:44 +0800

    fixed

--- app/Jobs/DoBid.php
+++ app/Jobs/DoBid.php
@@ -50,7 +50,7 @@ class DoBid implements ShouldQueue
             pp_log(" ".$bv['CreditCode']."开始投标",$bv['ListingId']);
             pp_bid_log('开始投标',$bv['ListingId'],$bv['CreditCode']);
             $request = '{"ListingId": '.$bv['ListingId'].',"Amount": 50,"UseCoupon":"true"}';
-            $result = json_decode($this->client->send($url, $request,10),true);
+            $result = json_decode($this->client->send($url, $request,30),true);
             if($result['Result']!= 0){
                 pp_bid_log($result['Result'].$result['ResultMessage'],$result['ListingId']);
                 pp_log($result['Result'].$result['ResultMessage'],$result['ListingId']);
