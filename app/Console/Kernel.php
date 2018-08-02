a21d339022f78a739e452e5e47379683702b7fc1
shitingle <694475241@qq.com>
2018-08-02 14:43:09 +0800

    fixed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\Ppdai'
     ];
 
     /**
@@ -24,8 +23,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('Ppdai')
-                  ->everyMinute();
     }
 
     /**
