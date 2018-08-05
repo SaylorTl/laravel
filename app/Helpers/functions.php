2cc065564c536e8e2f763b68e36dbc8d7fce7000
shitingle <shitingle@163.com>
2018-08-05 13:55:16 +0800

    fixed

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -14,11 +14,11 @@
 //    file_put_contents(dirname(dirname(__DIR__))."/storage/logs/".$day.".log","($now):".$creditcode."标号".$bid.$str."\n", FILE_APPEND);
 }
 
-function pp_bid_log($str,$bid=null,$creditcode=null){
+public function pp_bid_log($str,$bid=null,$creditcode=null){
     $now = date("Y-m-d H:i:s");
     echo "($now):".$creditcode."标号".$bid.$str."\n";
     $day = date("Y-m-d");
-    file_put_contents(dirname(dirname(__DIR__))."/storage/logs/bid_".$day.".log","($now):".$creditcode."标号".$bid.$str."\n", FILE_APPEND);
+    file_put_contents(dirname(dirname(dirname(__DIR__)))."/storage/logs/bid_".$day.".log","($now):".$creditcode."标号".$bid.$str."\n", FILE_APPEND);
 }
 
 function debet_bid_log($str,$bid=null,$creditcode=null){
